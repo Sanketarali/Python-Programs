d111f58bdbf6b6d046351b8e042da6fe8d3327e7
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2023-03-30 21:39:14 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,4 @@
+for i in range(0,6):
+    for j in range(1,6-i):
+        print("*" ,end="")
+    print()   
---
+++ 3rd.py
@@ -0,0 +1,6 @@
+k=1
+for i in range(0,6):
+    for j in range(1,i+1):
+        print(k ,'',end="")
+        k=k+1
+    print()   
