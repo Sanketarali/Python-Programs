8215b9dd9b5fb60561f98fe94b97e482c3cf6078
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2023-04-03 22:28:51 +0530

    Add files via upload

---
+++ 15-12/class.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self, fname,lname):
+        self.fname=fname
+        self.lname=lname
+        
+        
+
+
+sanket=Employee('sanket', 'arali')
+yatheesh=Employee('yatheesh','pateel')
+
+
+
+print(sanket.fname)
